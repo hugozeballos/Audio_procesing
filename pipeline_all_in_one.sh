86347b1fde32c0d8712e987c8dbe3536d4bbf280
hugozeballos <Hzeballosmeneses@gmail.com>
2025-09-29 12:24:58 -0400

    fix for scylla 1080 ti

--- pipeline_all_in_one.sh
+++ pipeline_all_in_one.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #SBATCH -J enh_benchmark
 #SBATCH --partition=ialab-high    # Se tiene que elegir una partición de nodos con GPU
-#SBATCH --gres=gpu:titan_rtx:1
+#SBATCH --gres=gpu:1080_ti:2
 #SBATCH -t 04:00:00
-#SBATCH --mem=32G
+#SBATCH --mem=16G
 #SBATCH -o logs/%x-%j.out
 #SBATCH -e logs/%x-%j.err
 #SBATCH --ntasks=1
